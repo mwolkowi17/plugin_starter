590df96d9626df12b4d02e3c49e21a6e72f000a6
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2025-05-20 10:03:37 +0200

    rest of plugin

--- extra-post-info.php
+++ extra-post-info.php
@@ -10,7 +10,7 @@ Version: 0.5 */
 if (!function_exists("extra_post_info")) {
     function extra_post_info($content)
     {
-        $extra_info = "EXTRA INFO";
+        $extra_info = get_option('extra_post_info');;
         return $content . $extra_info;
     }
     add_filter('the_content', 'extra_post_info');
@@ -29,6 +29,13 @@ if (!function_exists("extra_post_info_menu")) {
         $position   = 4;
 
         add_menu_page($page_title, $menu_title, $capability, $menu_slug, $function, $icon_url, $position);
+        add_action('admin_init', 'update_extra_post_info');
+        if (!function_exists("update_extra_post_info")) {
+            function update_extra_post_info()
+            {
+                register_setting('extra-post-info-settings', 'extra_post_info');
+            }
+        }
     }
 }
 
